a5a7d2049ab546bbe09bd329c64b58d630925fea
Jekyll-007 <teenofeejay@gmail.com>
2024-10-25 13:07:56 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def fibonacci_series(n):turn
+    series = []
+
+    if n < 1:
+        return series
+        elif n == 1:
+            return [0]
+
+    series = [0, 1]
+
+    while len(series) < n:
+        next_term = series[-1] + series[-2]
+        series_append(next_term)
+
+    return series
+
+num_terms = int(input("Enter the number of terms for the Fibonacci Series: "))
+result = fibonacci_series(num_terms)
+print("Fibonacci series up to", num_terms,"terms:", result)    
